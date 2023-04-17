f8df9e0fc106398c009ebff0ef7f6bf737988dc0
sansela <santosh.sela@gmail.com>
2023-04-16 22:38:24 -0400

    JS datatypes

---
+++ JS/1-js.js
@@ -0,0 +1,27 @@
+console.log("Hello from External file");
+
+let a =10;
+let b= 20;
+const PI = 3.173;
+let c;
+let d=null;
+let name = "Hello my  last name is sala";
+let last_name = "Sala"
+let hobbies = "Santhosh's Hobbies"
+let hobbies_backticks = `Santhosh's Hobbies"`
+
+// PI=1; It errors out
+
+console.log(a+b); //Num + Num = 30
+console.log(a+true); //Num+ Boolean = 11
+console.log(a+"20"); //Num + String = 1020
+console.log(PI); //3.173
+console.log(c); //undefined
+console.log(d); //null
+console.log(typeof(a)); //number
+console.log(typeof(d)); //object
+
+console.log(name);
+console.log(`Hello my last name is ${last_name}`);
+console.log(hobbies);
+console.log(hobbies_backticks);
