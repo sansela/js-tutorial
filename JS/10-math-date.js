2c74dc480aa3b0265f26a01f97e1a3a768b3b245
sansela <santosh.sela@gmail.com>
2023-04-26 00:48:37 -0400

    Math and Date objects

---
+++ JS/10-math-date.js
@@ -0,0 +1,66 @@
+let a;
+// a = Math.round(5.4);
+// a = Math.round(5.6);
+// a = Math.round(5);
+
+// a=Math.floor(5.4);
+// a=Math.floor(5.6);
+// a= Math.floor(5);
+
+// a = Math.ceil(5.4);
+// a = Math.ceil(5);
+
+// a = Math.abs(6.5);
+// a = Math.abs(-6.5);
+
+// a = Math.sign(5);
+// a = Math.sign(-5);
+// a = Math.sign(0);
+
+// a = Math.pow(2, 3);
+// a = Math.pow(2, 10);
+
+// a = Math.sqrt(25);
+// a = Math.sqrt(6);
+
+// a = Math.log2(8);
+// a = Math.log2(9);
+
+// a = Math.log10(100);
+// a = Math.log10(2);
+
+// a = Math.sin(degrees(30)); //deg = (PI* theeta)/180
+// a = Math.sin(degrees(45));
+// a = Math.sin(degrees(90));
+// a = Math.cos(degrees(0));
+
+// a = Math.random();
+// a = Math.random() * 10 + 1;
+// a = Math.floor(Math.random() * 10 + 1); //1 -10
+// a = Math.floor(Math.random() * 15 + 1); //1-15
+
+// a = Math.floor(Math.random() * 6 + 6); // >=6
+// console.log(a);
+
+let date;
+// date = new Date();
+// date = ("Wed Apr 26 2023 00:37:28");
+// date = new Date("2022 5 1"); //Sun May 01 2022 00:00:00 GMT-0400 (Eastern Daylight Time)
+// date = new Date("2022/4/1"); //Fri Apr 01 2022 00:00:00 GMT-0400 (Eastern Daylight Time)
+// date = new Date("2022/5/1 12:12:12:12"); //Sun May 01 2022 12:12:12 GMT-0400 (Eastern Daylight Time)
+// date = new Date("2022/13/13"); //Invalid Date
+// date = new Date();
+
+// console.log(date);
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getFullYear());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+// console.log(date.getMilliseconds());
+// console.log(date.toUTCString());
+
+function degrees(angle) {
+    return (Math.PI * angle) /180;
+}
