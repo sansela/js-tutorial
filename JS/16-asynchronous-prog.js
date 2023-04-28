2ad2516eba452267cc8e8a0b1658236cdd0a659d
sansela <santosh.sela@gmail.com>
2023-04-27 22:57:28 -0400

    async programming

---
+++ JS/16-asynchronous-prog.js
@@ -0,0 +1,11 @@
+//Asynchrousnous programming - CAP
+// 1. Callbacks
+// 2. Promises
+// 3. async/await
+
+//one way is using setTimeout function
+
+setTimeout(() => {
+    console.log("setTimeout console log");
+},5000)
+console.log("Last Line");
