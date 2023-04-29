791160b030de0a72037ce7b05094038579dda42d
sansela <santosh.sela@gmail.com>
2023-04-28 23:02:49 -0400

    callbacks and callback hell

---
+++ JS/18-callbacks.js
@@ -0,0 +1,26 @@
+//Callback functions - function which is passed as an argument to another function.
+//Callback hell - multiple call backs creates havoc.
+
+const arr = [1, 2, 3, 4, 5];
+
+calculate = (callback) => {
+   const calc = [];
+  for(i=0; i< arr.length; i++) {
+    calc.push(callback(arr[i]));
+  }
+  console.log(calc);
+
+}
+
+// function multiply(a) {
+//     return a*10;
+// }
+
+// function divide(a) {
+//     return a/10;
+// }
+
+// calculate(multiply);
+// calculate(divide);
+calculate(a => a*10);
+calculate(a => a/10);
