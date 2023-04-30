c5c4178fba77e8a995b6a629fdc091bf15489d17
sansela <santosh.sela@gmail.com>
2023-04-30 00:35:12 -0400

    BOM

---
+++ JS/22-BOM.js
@@ -0,0 +1,23 @@
+// Window
+//     Navigator
+//     Document
+//     History
+//     Screen
+//     Location
+
+// console.log(navigator);
+// console.log(navigator.geolocation.getCurrentPosition(position => console.log(position)));
+
+// console.log(history);
+
+// console.log(location);
+// console.log(location.hostname);
+// console.log(location.host);
+// console.log(location.href);
+// console.log(location.pathname);
+// console.log(location.port);
+// console.log(location.protocol);
+// console.log(location.search); //search query
+// location.reload();
+// location.replace("https://youtube.com"); //new page with back button.
+// location.assign("https://youtube.com"); new page without any back/forward button
